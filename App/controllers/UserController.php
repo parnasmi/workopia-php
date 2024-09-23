9e1024a716b76a8c6b1a83dfd1020b6bebd348c9
Ilhom Maksadkulov <ilhommaksadkulov@Ilhoms-MacBook-Pro.local>
2024-09-23 07:15:55 +0500

    feature: implement login

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -131,4 +131,68 @@ class UserController {
         setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
         redirect('/');
     }
+
+    /**
+     * Login with email and password
+     *
+     * @return void
+     */
+    public function authenticate(): void {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $errors = [];
+
+        //Validation
+        if (!Validation::email($email)) {
+            $errors['email'] = 'Please, enter a valid email';
+        }
+
+        if (!Validation::string($password, 6, 50)) {
+            $errors['password'] = 'Password must be at least 6 characters';
+        }
+
+        if (!empty($errors)) {
+            loadView('users/login', [
+                'errors' => $errors
+            ]);
+            exit;
+        }
+
+        //Chexk for email
+        $params = [
+            'email' => $email
+        ];
+
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+
+        if (!$user) {
+            $errors['email'] = 'Incorrect credentials';
+            loadView('users/login', [
+                'errors' => $errors
+            ]);
+            exit;
+        }
+
+        //Check if password is correct
+        if (!password_verify($password, $user->password)) {
+            $errors['email'] = 'Incorrect credentials';
+            loadView('users/login', [
+                'errors' => $errors
+            ]);
+            exit;
+        }
+
+        //Set user session
+
+        Session::set('user', [
+            'id' => $user->id,
+            'name' => $user->name,
+            'city' => $user->city,
+            'email' => $user->email,
+            'state' => $user->state,
+        ]);
+
+        redirect('/');
+    }
 }
